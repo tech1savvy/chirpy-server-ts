230cf6ace8b56909f9aafe31a9f377c1d351d23b
tech1savvy <amankumar010604@gmail.com>
2026-01-26 23:14:30 +0530

    feat: add .env file support with dbURL env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 type ApiConfig = {
   fileserverhits: number;
+  dbURL: string;
 };
 
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+  if (process.env[key] === undefined) {
+    throw new Error(`${key}; Environment variable is missing.`);
+  }
+  return process.env[key];
+}
+
+envOrThrow("DB_URL");
 export const config: ApiConfig = {
   fileserverhits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
