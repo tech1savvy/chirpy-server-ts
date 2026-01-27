5a3177f1cf59a9c5c197835fb59670913b924ba6
tech1savvy <amankumar010604@gmail.com>
2026-01-27 10:07:13 +0530

    feat(api/users): add handler for creating a new user

---
+++ src/api/users.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from "express";
+import { createUser } from "../db/queries/users.js";
+import { BadRequestError } from "../errors.js";
+import { respondWithJSON } from "../utils/json.js";
+
+export async function handlerUsersCreate(req: Request, res: Response) {
+  type parameters = {
+    email: string;
+  };
+
+  const params: parameters = req.body;
+
+  if (!params.email) {
+    throw new BadRequestError("Email not provided");
+  }
+
+  const user = await createUser({
+    email: params.email,
+  });
+
+  if (!user) {
+    throw new Error("Could not create user");
+  }
+
+  return respondWithJSON(res, 201, {
+    id: user.id,
+    email: user.email,
+    createAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  });
+}
