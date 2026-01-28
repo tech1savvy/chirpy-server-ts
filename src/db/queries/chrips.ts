ab5b83365d665e759165a56ce00e589af1b501bb
tech1savvy <amankumar010604@gmail.com>
2026-01-28 09:43:55 +0530

    feat(db): add query to get all chirps

--- src/db/queries/chrips.ts
+++ src/db/queries/chrips.ts
@@ -1,7 +1,13 @@
+import { asc } from "drizzle-orm";
 import { db } from "../index.js";
 import { NewChirp, chirps } from "../schema.js";
 
 export async function createChirp(chirp: NewChirp) {
   const [row] = await db.insert(chirps).values(chirp).returning();
   return row;
+}
+
+export async function getChirp() {
+  const rows = await db.select().from(chirps).orderBy(asc(chirps.createdAt));
+  return rows;
 }
