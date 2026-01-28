0075731fe91e26c775b0cf8d1bc2a8a1114430b4
tech1savvy <amankumar010604@gmail.com>
2026-01-28 07:43:53 +0530

    feat(api/chirps): add handler for create valid chirp
    - refactored old validateChirp handler logic inside createChirp handler

--- src/api/chirp.ts
+++ src/api/chirp.ts
@@ -1,37 +1,55 @@
 import type { Request, Response } from "express";
-import { respondWithError, respondWithJSON } from "../utils/json.js";
 import { BadRequestError } from "../errors.js";
+import { createChirp } from "../db/queries/chrips.js";
+import { respondWithJSON } from "../utils/json.js";
 
-export function handlerValidateChirp(req: Request, res: Response) {
+export async function handlerChirpsCreate(req: Request, res: Response) {
   type parameters = {
     body: string;
+    userId: string;
   };
 
   const params: parameters = req.body;
-  if (!params.body || typeof params.body !== "string") {
-    return respondWithError(res, 400, "Something went wrong");
+  if (!params.body) {
+    throw new BadRequestError("Body is missing");
+  }
+  if (!params.userId) {
+    throw new BadRequestError("User ID is missing");
   }
 
-  const maxChirpLength = 140;
-  if (params.body.length > maxChirpLength) {
-    throw new BadRequestError(
-      `Chirp is too long. Max length is ${maxChirpLength}`,
-    );
-  }
+  const cleanBody = clean(validate(params.body));
 
-  const profaneWords = ["kerfuffle", "sharbert", "fornax"];
-  const chirpWords = params.body.split(" ");
-  console.log(chirpWords);
-  const cleandWords = chirpWords.map((word) => {
-    const lowercaseWord = word.toLowerCase();
-    if (profaneWords.includes(lowercaseWord)) {
-      return "****";
+  const chirp = await createChirp({
+    body: cleanBody,
+    userId: params.userId,
+  });
+
+  return respondWithJSON(res, 201, {
+    id: chirp.id,
+    createAt: chirp.createdAt,
+    updatedAt: chirp.updaetdAt,
+    body: chirp.body,
+    userId: chirp.userId,
+  });
+
+  function validate(chirp: string) {
+    const maxChirpLength = 140;
+    if (chirp.length > maxChirpLength) {
+      throw new BadRequestError(
+        `Chirp is too long. Max length is ${maxChirpLength}`,
+      );
     }
-    return word;
-  });
-  console.log(cleandWords);
-
-  return respondWithJSON(res, 200, {
-    cleanedBody: cleandWords.join(" "),
-  });
+    return chirp;
+  }
+  function clean(chirp: string) {
+    const profaneWords = ["kerfuffle", "sharbert", "fornax"];
+    const cleanWords = chirp.split(" ").map((word) => {
+      const lowercaseWord = word.toLowerCase();
+      if (profaneWords.includes(lowercaseWord)) {
+        return "****";
+      }
+      return word;
+    });
+    return cleanWords.join(" ");
+  }
 }
