8b7a69c54a1a6b9b247d2dc0ab1e595053be56b1
tech1savvy <amankumar010604@gmail.com>
2026-02-06 16:56:49 +0530

    fix: remove explicity types and promise rewrapping with await keyword
    async functions

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,13 +1,9 @@
 import argon2 from "argon2";
 
-export async function hashPassword(password: string): Promise<string> {
-  const hash = await argon2.hash(password);
-  return hash;
+export async function hashPassword(password: string) {
+  return argon2.hash(password);
 }
 
-export async function checkPasswordHash(
-  password: string,
-  hash: string,
-): Promise<boolean> {
-  return await argon2.verify(hash, password);
+export async function checkPasswordHash(password: string, hash: string) {
+  return argon2.verify(hash, password);
 }
