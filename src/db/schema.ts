6495ad8437b0ae6aa891336a601b1339a716301a
tech1savvy <amankumar010604@gmail.com>
2026-02-06 16:44:54 +0530

    fix: add length constraint to hashed_password column

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,7 +8,9 @@ export const users = pgTable("users", {
     .defaultNow()
     .$onUpdate(() => new Date()),
   email: varchar("email", { length: 256 }).unique().notNull(),
-  hashed_password: varchar("hashed_password").notNull().default("unset"),
+  hashed_password: varchar("hashed_password", { length: 256 })
+    .notNull()
+    .default("unset"),
 });
 
 export type NewUser = typeof users.$inferInsert;
