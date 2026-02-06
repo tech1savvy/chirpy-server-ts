8736805e52a04368ecfbbcc9bac8de6b2ae59095
tech1savvy <amankumar010604@gmail.com>
2026-02-06 16:33:41 +0530

    fix(api/auth): omit hashed_Password field form handlerLogin res

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,12 @@
 import type { Request, Response } from "express";
 import { getUserByEmail } from "../db/queries/users.js";
-import { NewUser } from "../db/schema.js";
 import { BadRequestError, Unauthorised } from "../errors.js";
 import { checkPasswordHash } from "../utils/auth.js";
 import { respondWithJSON } from "../utils/json.js";
+import { users } from "../db/schema.js";
+
+type User = typeof users.$inferSelect;
+type UserResponse = Omit<User, "hashed_password">;
 
 export async function handlerLogin(req: Request, res: Response) {
   type parameters = {
@@ -33,6 +36,8 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new Unauthorised("incorrect email or password");
   }
 
-  const resUser: Omit<NewUser, "hashed_password"> = user;
+  const { hashed_password, ...restUser } = user;
+  const resUser: UserResponse = restUser;
+
   return respondWithJSON(res, 200, resUser);
 }
