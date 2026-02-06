f66926fe7822cda7ca407f9d8ffa34120b98ca6d
tech1savvy <amankumar010604@gmail.com>
2026-02-06 15:09:43 +0530

    fix(api/users): omit hashed_password field from handlerUsersCreate

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,10 +2,13 @@ import type { Request, Response } from "express";
 import { createUser } from "../db/queries/users.js";
 import { BadRequestError } from "../errors.js";
 import { respondWithJSON } from "../utils/json.js";
+import { hashPassword } from "../utils/auth.js";
+import { NewUser } from "../db/schema.js";
 
 export async function handlerUsersCreate(req: Request, res: Response) {
   type parameters = {
     email: string;
+    password: string;
   };
 
   const params: parameters = req.body;
@@ -13,19 +16,18 @@ export async function handlerUsersCreate(req: Request, res: Response) {
   if (!params.email) {
     throw new BadRequestError("Email not provided");
   }
+  if (!params.password) {
+    throw new BadRequestError("Password not provided");
+  }
 
-  const user = await createUser({
+  const user: Omit<NewUser, "hash_password"> = await createUser({
     email: params.email,
+    hashed_password: await hashPassword(params.password),
   });
 
   if (!user) {
     throw new Error("Could not create user");
   }
 
-  return respondWithJSON(res, 201, {
-    id: user.id,
-    email: user.email,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt,
-  });
+  return respondWithJSON(res, 201, user);
 }
