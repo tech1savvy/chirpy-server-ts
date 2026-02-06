f73f615803c2998d5daac9c8175819b44ffa6fde
tech1savvy <amankumar010604@gmail.com>
2026-02-06 15:42:54 +0530

    fix(auth): use Unauthorised error class

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
 import { getUserByEmail } from "../db/queries/users.js";
 import { NewUser } from "../db/schema.js";
-import { BadRequestError } from "../errors.js";
+import { BadRequestError, Unauthorised } from "../errors.js";
 import { checkPasswordHash } from "../utils/auth.js";
-import { respondWithError, respondWithJSON } from "../utils/json.js";
+import { respondWithJSON } from "../utils/json.js";
+
 export async function handlerLogin(req: Request, res: Response) {
   type parameters = {
     email: string;
@@ -21,14 +22,17 @@ export async function handlerLogin(req: Request, res: Response) {
 
   const user = await getUserByEmail(params.email);
   if (!user) {
-    return respondWithError(res, 401, "incorrect email or password");
+    throw new Unauthorised("incorrect email or password");
   }
 
-  const isAuthentic = checkPasswordHash(params.password, user.hashed_password);
+  const isAuthentic = await checkPasswordHash(
+    params.password,
+    user.hashed_password,
+  );
   if (!isAuthentic) {
-    return respondWithError(res, 401, "incorrect email or password");
+    throw new Unauthorised("incorrect email or password");
   }
 
   const resUser: Omit<NewUser, "hashed_password"> = user;
-  respondWithJSON(res, 200, resUser);
+  return respondWithJSON(res, 200, resUser);
 }
