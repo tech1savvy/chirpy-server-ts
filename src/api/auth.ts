0a1d907869cebf02492ff59bbe36e1eb5915551c
tech1savvy <amankumar010604@gmail.com>
2026-02-07 17:15:33 +0530

    feat(api/auth): issue JWT token upon successful login
      - Add expiresInSeconds parameter to login request with a 1-hour cap.
      - Generate a JWT using the application's secret key.
      - Include the generated token in the login response payload.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,6 +4,8 @@ import { BadRequestError, Unauthorised } from "../errors.js";
 import { checkPasswordHash } from "../utils/auth.js";
 import { respondWithJSON } from "../utils/json.js";
 import { users } from "../db/schema.js";
+import { makeJWT } from "../utils/jwt.js";
+import { config } from "../config.js";
 
 type User = typeof users.$inferSelect;
 type UserResponse = Omit<User, "hashed_password">;
@@ -12,6 +14,7 @@ export async function handlerLogin(req: Request, res: Response) {
   type parameters = {
     email: string;
     password: string;
+    expiresInSeconds?: number;
   };
 
   const params: parameters = req.body;
@@ -22,12 +25,17 @@ export async function handlerLogin(req: Request, res: Response) {
   if (!params.password) {
     throw new BadRequestError("Password not provided");
   }
+  if (!params.expiresInSeconds || params.expiresInSeconds > 1 * 60 * 60) {
+    params.expiresInSeconds = 1 * 60 * 60;
+  }
 
+  // Fetch User
   const user = await getUserByEmail(params.email);
   if (!user) {
     throw new Unauthorised("incorrect email or password");
   }
 
+  // Match Password Hash
   const isAuthentic = await checkPasswordHash(
     params.password,
     user.hashed_password,
@@ -36,8 +44,11 @@ export async function handlerLogin(req: Request, res: Response) {
     throw new Unauthorised("incorrect email or password");
   }
 
+  // Make JWT
+  const token = makeJWT(user.id, config.api.jwtSecret, params.expiresInSeconds);
+
   const { hashed_password, ...restUser } = user;
   const resUser: UserResponse = restUser;
 
-  return respondWithJSON(res, 200, resUser);
+  return respondWithJSON(res, 200, { ...resUser, token });
 }
